9c0a9526153b3017601e5548f9d482799639e866
vicenttcarvalho <vicentecleyton@gmail.com>
2023-06-23 11:55:31 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+  const logo = document.querySelector("footer img")
+  //substituir imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, mudar para imagem do light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    logo.setAttribute("src", "./assets/logo-light.png")
+  } else {
+    //se tiver dark mode, manter imagem padrão
+    img.setAttribute("src", "./assets/avatar-dark.png")
+    logo.setAttribute("src", "./assets/logo-dark.png")
+  }
+}
